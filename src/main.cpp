6c4199d868440ce12c6047c24075ec1e1d4a6830
whitelies125 <lzb125125@gmail.com>
2025-08-26 05:22:36 +0800

    init. 配环境ok

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+// clangd-format off
+#include <glad/glad.h>  // 这个头文件得放在最前面
+// clangd-format on
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    return 0;
+}
